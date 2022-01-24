09b89222e00e89c8b9116aa79d361db053e6d0d5
Haoran Zhou <kens0serious@gmail.com>
2022-01-23 23:16:50 -0500

    completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,75 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
-  return <h2>tabs project setup</h2>
+  const [jobs, setJobs] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    try {
+      const res = await fetch(url)
+      const jobs = await res.json()
+      setJobs(jobs)
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchJobs()
+  }, [])
+
+  if (loading) {
+    return (
+      <main>
+        <h1>Loading</h1>
+      </main>
+    )
+  }
+
+  const { title, dates, duties, company } = jobs[value]
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map(({ company, id }, index) => {
+            return (
+              <button
+                className={index === value ? 'job-btn active-btn' : 'job-btn'}
+                key={id}
+                onClick={() => {
+                  setValue(index)
+                }}
+              >
+                {company}
+              </button>
+            )
+          })}
+        </div>
+
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{duty}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <button className="btn">more info</button>
+    </section>
+  )
 }
 
 export default App
